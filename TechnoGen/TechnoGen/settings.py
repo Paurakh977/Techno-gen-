a7d731418218854086cee222f1780eb2dc8f9fb8
“Paurakh977” <pandeypaurakhraj@gmail.com>
2024-12-10 00:11:16 +0545

    setting setups

--- TechnoGen/TechnoGen/settings.py
+++ TechnoGen/TechnoGen/settings.py
@@ -127,11 +127,12 @@ CORS_ALLOWED_METHODS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'technogen_db'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        
     }
 }
 
